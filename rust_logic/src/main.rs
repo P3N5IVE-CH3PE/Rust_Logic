457360abbe187ff16fcd8faaa2c89e6175feb753
P3N5IVE_CH3PE <P3N5IVE_CH3PE@protonmail.com>
2024-09-10 20:47:31 -0700

    End of function lesson.

--- rust_logic/src/main.rs
+++ rust_logic/src/main.rs
@@ -1,12 +1,43 @@
 fn main() {
-    let number = 100;
+    	//let condition = false;
 
-    if number < 20 {
-	println!("condition was true");
-} else {
-	println!("condition was false");
+	//let number =  if condition {5} else {6};
 
+
+	//println!("The value of number is:{number}");
+
+	//	let x = 1;
+
+	//  	let y = if x { 0 } else { 1 };
+ 
+	//  	println!("{y}");
+	
+
+	//	loop {println!("Again!");}
+
+			
+
+	let a = [100,200,300,400,500];
+	let mut index = 0;
+	
+	while index < 5 {
+		println!("The value is: {}",a[index]);
+		
+		index += 1;
+	
 	}
 
 
+	let a = [10,20,30,40,50];
+
+	for element in a {
+	
+	println!("The value is: {element}");
+	}
+
+
+	for number in (1..4).rev(){
+		println!("{number}")
+	}
+	println!("Take Off!");
 }
